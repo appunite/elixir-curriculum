03416ac184572a95e2aaecaaf6a3434c6d0fb279
damian.romanow <damian.romanow@gmail.com>
2024-03-13 15:41:26 +0100

    fixed mix elixir blocks

--- example_projects/pokemon_profile/lib/pokemon_profile/greeter.ex
+++ example_projects/pokemon_profile/lib/pokemon_profile/greeter.ex
@@ -1,13 +1,5 @@
-# defmodule PokemonProfile.Greeter do
-#   def hello(pokemon_name) do
-#     "Hello #{pokemon_name}!"
-#   end
-# end
-
 defmodule PokemonProfile.Greeter do
-  @callback hello(String.t()) :: String.t()
-
-  @adapter Application.compile_env!(:pokemon_profile, :adapter)
-
-  defdelegate hello(pokemon_name), to: @adapter
+  def hello(pokemon_name) do
+    "Hello #{pokemon_name}!"
+  end
 end
--- example_projects/pokemon_umbrella/apps/pokemon_profile/lib/pokemon_profile/greeter/formal.ex
+++ example_projects/pokemon_umbrella/apps/pokemon_profile/lib/pokemon_profile/greeter/formal.ex
@@ -1,4 +1,6 @@
 defmodule PokemonProfile.Greeter.Formal do
+  @behaviour PokemonProfile.Greeter
+
   def hello(pokemon_name) do
     "Good morning #{pokemon_name}."
   end
